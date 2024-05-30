3e56a18a3ba96f512769ea041a836fc64393f962
yikim <156034986+ilyeongkim@users.noreply.github.com>
2024-05-30 17:49:46 +0900

    2024.05.30

---
+++ blog/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  // eslint-disable-next-line no-undef
+  siteUrl: process.env.SITE_URL || 'https://vercel-blog-beta-neon.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -3,6 +3,9 @@ import Layout from '../../components/Layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/Date'
 import utilStyles from '../../styles/utils.module.css'
+import { MDXRemote } from 'next-mdx-remote'
+import { Children } from 'react'
+import CodeBlock from '../../components/CodeBlock'
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -21,6 +24,19 @@ export async function getStaticProps({ params }) {
   }
 }
 
+const Button = ({ children }) => {
+  return (
+    <button
+      className="bg-black dark:bg-white text-lg text-teal-200 dark:text-teal-700 rounded-lg px-5"
+      onClick={() => alert(`thanks to ${children}`)}
+    >
+      {children}
+    </button>
+  )
+}
+
+const components = { Button, CodeBlock }
+
 export default function Post({ postData }) {
   return (
     <Layout>
@@ -30,7 +46,12 @@ export default function Post({ postData }) {
           <Date dateString={postData.date} />
         </div>
         <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        {postData.contentHtml && (
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        )}
+        {postData.mdxSource && (
+          <MDXRemote {...postData.mdxSource} components={components} />
+        )}
       </article>
     </Layout>
   )
